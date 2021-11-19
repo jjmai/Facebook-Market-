623e2b042c0c2f922eb544dfc8cb206c20ee0848
Maxwell Mollison <mmolliso@ucsc.edu>
2021-11-19 07:45:49 -0800

    Initial commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Dummy from './Dummy';
+
+/**
+ * Simple component with no state.
+ *
+ * @return {object} JSX
+ */
+function App() {
+  return (
+    <Dummy />
+  );
+}
+
+export default App;
